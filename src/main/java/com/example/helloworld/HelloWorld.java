f00164ab5e02191c4632c2c3c80c812f647ff085
Shravankumar1989 <Shravansahan@gmail.com>
2024-06-16 22:40:30 +0800

    Add Apache Commons Lang dependency and update HelloWorld.java

--- src/main/java/com/example/helloworld/HelloWorld.java
+++ src/main/java/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.example.helloworld;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String message = "hello, world!";
+        String capitalizedMessage = StringUtils.capitalize(message);
+        System.out.println(capitalizedMessage);
     }
 }
